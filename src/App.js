75665a0632d32509f66b6952e680dbafb5825bb0
JoaoMateus23 <celsoblack201@gmail.com>
2021-03-06 17:38:53 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'; 
+
+function App() {
+  return (
+    <div className="App">
+      <p>mesmo com tudo isso ainda gosto dela</p>
+    </div>
+  );
+}
+
+export default App;
